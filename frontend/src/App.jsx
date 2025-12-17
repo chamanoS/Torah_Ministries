2e95571479022265713ebf4043074b2c5e1e832e
Chamano Ramashia <chamano.sr@gmail.com>
2025-12-17 08:18:13 +0200

    setting up frontend, installing dependancies, tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+       <h1 className="text-3xl font-bold text-blue-600">
+        Tailwind is working
+      </h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
