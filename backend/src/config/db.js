df91376befb38755d771a269e9b378ba9487205f
Chamano Ramashia <chamano.sr@gmail.com>
2025-12-17 07:52:04 +0200

    configuring the db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,8 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
   server: process.env.DB_SERVER,
   database: process.env.DB_NAME,
   options: {
